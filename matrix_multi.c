4b6d05c7010cbc8920a4cd84fa86c3acd5b69ed8
rajpootrahul6113 <56197243+rajpootrahul6113@users.noreply.github.com>
2019-10-05 22:08:16 +0530

    Create matrix_multi.c

---
+++ matrix_multi.c
@@ -0,0 +1,68 @@
+#include<stdio.h>    
+#include<stdlib.h>  
+int main(){  
+int a[10][10],b[10][10],mul[10][10],r,c,i,j,k;    
+system("cls");  
+printf("enter the number of row=");    
+scanf("%d",&r);    
+printf("enter the number of column=");    
+scanf("%d",&c);    
+printf("enter the first matrix element=\n");    
+for(i=0;i<r;i++)    
+{    
+for(j=0;j<c;j++)    
+{    
+scanf("%d",&a[i][j]);    
+}    
+}    
+printf("enter the second matrix element=\n");    
+for(i=0;i<r;i++)    
+{    
+for(j=0;j<c;j++)    
+{    
+scanf("%d",&b[i][j]);    
+}    
+}    
+    
+printf("multiply of the matrix=\n");    
+for(i=0;i<r;i++)    
+{    
+for(j=0;j<c;j++)    
+{    
+mul[i][j]=0;    
+for(k=0;k<c;k++)    
+{    
+mul[i][j]+=a[i][k]*b[k][j];    
+}    
+}    
+}    
+//for printing result    
+for(i=0;i<r;i++)    
+{    
+for(j=0;j<c;j++)    
+{    
+printf("%d\t",mul[i][j]);    
+}    
+printf("\n");    
+}    
+return 0;  
+}  
+
+/*
+output-
+
+enter the number of row=3
+enter the number of column=3
+enter the first matrix element=
+1 1 1
+2 2 2
+3 3 3
+enter the second matrix element=
+1 1 1
+2 2 2
+3 3 3
+multiply of the matrix=
+6 6 6
+12 12 12
+18 18 18
+*/
