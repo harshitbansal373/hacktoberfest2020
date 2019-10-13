87006aa62ad707c6ec24393ef1831ae21bd932af
Kunal Gupta <43882773+kunal0287@users.noreply.github.com>
2019-10-13 21:30:02 +0530

    Update armstrong.c

--- armstrong.c
+++ armstrong.c
@@ -1,22 +1,22 @@
 #include<stdio.h>  
 int main()    
 {    
-int n,r,sum=0,temp;    
-printf("enter the number=");    
-scanf("%d",&n);    
-temp=n;    
-while(n>0)    
-{    
-r=n%10;    
-sum=sum+(r*r*r);    
-n=n/10;    
-}    
-if(temp==sum)    
-printf("armstrong  number ");    
-else    
-printf("not armstrong number");    
-return 0;  
-}   
+  int n,r,sum=0,temp;    
+  printf("enter the number=");    
+  scanf("%d",&n);    
+  temp=n;    
+  while(n>0)    
+  {    
+    r=n%10;    
+    sum=sum+(r*r*r);    
+    n=n/10;    
+  }    
+  if(temp==sum)    
+    printf("armstrong  number ");    
+  else    
+    printf("not armstrong number");    
+  return 0;  
+  }   
 
 /*
 Output:
