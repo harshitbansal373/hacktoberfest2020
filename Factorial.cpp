bb98873102a45ebb3a5e3701baf235be7f9da33b
Unnati Mishra <54355445+UnnatiMishra@users.noreply.github.com>
2019-10-23 00:52:08 +0530

    Create Factorial.cpp

---
+++ Factorial.cpp
@@ -0,0 +1,16 @@
+#include<iostream.h>
+#include<conio.h>
+
+void main()
+{
+int i, no, fact=1;
+clrscr();
+cout<<"Enter the any no. : ";
+cin>>no;
+for(i=1;i<=no;i++)
+{
+fact=fact*i;
+}
+cout<<"Factorial: "<<fact;
+getch();
+}
