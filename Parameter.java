c9ec255b4d95168970d97eab855e8b6719ae71eb
Mohamed Adam Mohamed Khan <39467230+mokhancodes@users.noreply.github.com>
2019-10-31 18:35:52 +0300

    Parameter.java
    Sample code for Parameter in Java.

---
+++ Parameter.java
@@ -0,0 +1,18 @@
+public class Example { 
+  
+    // the variables a and b are parameters 
+    public static int multiply(int a, int b) 
+    { 
+        return a + b; 
+    } 
+  
+    public static void main(String[] args) 
+    { 
+        int x = 2; 
+        int y = 5; 
+  
+        int sum = multiply(x, y); 
+  
+        System.out.println("SUM IS: " + sum); 
+    } 
+}
