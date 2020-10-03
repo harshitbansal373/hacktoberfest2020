11f7bfb17432b22c9f892bfec6660a5a20ab412c
Bhagyasri1310 <71930084+Bhagyasri1310@users.noreply.github.com>
2020-10-03 20:27:31 +0530

    Create Selection sort.c

---
+++ Selection sort.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+int main()
+{
+int a[100], n, i, j, position, swap;
+printf("Enter number of elementsn");
+scanf("%d", &n);
+printf("Enter %d Numbersn", n);
+for (i = 0; i < n; i++)
+scanf("%d", &a[i]);
+for(i = 0; i < n - 1; i++)
+{
+position=i;
+for(j = i + 1; j < n; j++)
+{
+if(a[position] > a[j])
+position=j;
+}
+if(position != i)
+{
+swap=a[i];
+a[i]=a[position];
+a[position=swap;
+}
+}
+printf("Sorted Array:n");
+for(i = 0; i < n; i++)
+printf("%dn", a[i]);
+return 0;
+}
