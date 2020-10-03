d568da061b9c12c28a0f92c0bd285fbe2f91933b
jainabhi2001 <55582546+jainabhi2001@users.noreply.github.com>
2020-10-03 15:43:45 +0530

    Update Fibonacci.c
    It consists of simple fibonacci code without recurrsion.

--- Fibonacci.c
+++ Fibonacci.c
@@ -1,23 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 void fibonacci(int num)
-{
-	if(num == 1 || num == 2)printf("1\n");
-	else
-	{
-		int *fib = (int *)malloc(num*sizeof(int));
-		fib[0] = 1;
-		fib[1] = 1;
-		for(int i = 2 ; i < num ; i++)
-			fib[i] = fib[i-1] + fib[i-2];
-		for(int i = 0; i < num ; i++)
-			printf("%d\n" , fib[i]);
-	}
+{	int f=0;		//first element
+	int s=1;		//second element		
+	int t;			//third element
+	printf("\n Fibonacci series : %d %d ",f,s);
+	for( int i=2;i<num;i++)
+		{t=f+s;		//third element is the sum of first and second element
+		 printf("%d ",t);
+		 f=s;		//now assigning second elment to first
+		 s=t;		//now assigning third elment to second
+		}
 }
-void main()
+int main()
 {
 	printf("Enter Number of Elements : ");
 	int num;
 	scanf("%d" , &num);
 	fibonacci(num);
+	return 0;
 }
