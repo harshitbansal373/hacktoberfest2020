98833e62c6f3d95c653d8d7844e3d0049b5c7964
EH-GD-MOHIT21 <70685094+EH-GD-MOHIT21@users.noreply.github.com>
2020-10-04 13:03:10 +0530

    Update Armstrong.py

--- Armstrong.py
+++ Armstrong.py
@@ -1,9 +1,16 @@
+'''
+    1634 is an armstrong number because
+    1**4+6**4+3**4+4**4 == 1634
+    153 :-> 1**3 + 5**3 + 3**3
+
+'''
+
 num = int(input("Enter a number: "))
 sum = 0
 temp = num
 while temp > 0:
    digit = temp % 10
-   sum += digit ** 3
+   sum += digit ** len(str(num))    #power should be len of number not 3
    temp //= 10
 if num == sum:
    print(num,"is an Armstrong number")
