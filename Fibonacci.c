c4dec0d1a0ae2644be0f280e2a075561e1318042
Aashish2023 <63571854+Aashish2023@users.noreply.github.com>
2020-10-04 21:24:06 +0530

    Update Fibonacci.c

--- Fibonacci.c
+++ Fibonacci.c
@@ -1,23 +1,16 @@
 #include <stdio.h>
-#include <stdlib.h>
-void fibonacci(int num)
-{
-	if(num == 1 || num == 2)printf("1\n");
-	else
-	{
-		int *fib = (int *)malloc(num*sizeof(int));
-		fib[0] = 1;
-		fib[1] = 1;
-		for(int i = 2 ; i < num ; i++)
-			fib[i] = fib[i-1] + fib[i-2];
-		for(int i = 0; i < num ; i++)
-			printf("%d\n" , fib[i]);
-	}
-}
-void main()
-{
-	printf("Enter Number of Elements : ");
-	int num;
-	scanf("%d" , &num);
-	fibonacci(num);
+int main() {
+    int i, n, t1 = 0, t2 = 1, nextTerm;
+    printf("Enter the number of terms: ");
+    scanf("%d", &n);
+    printf("Fibonacci Series: ");
+
+    for (i = 1; i <= n; ++i) {
+        printf("%d, ", t1);
+        nextTerm = t1 + t2;
+        t1 = t2;
+        t2 = nextTerm;
+    }
+
+    return 0;
 }
