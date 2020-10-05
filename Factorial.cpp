b1a0c6ae0f3bd66ef6befff9d4a20117cc06fc99
Dv-888 <71933184+Dv-888@users.noreply.github.com>
2020-10-05 13:21:37 +0530

    added condition for  factorial of 0

--- Factorial.cpp
+++ Factorial.cpp
@@ -7,9 +7,17 @@ int i, no, fact=1;
 clrscr();
 cout<<"Enter the any no. : ";
 cin>>no;
-for(i=1;i<=no;i++)
+if(no==0)
+{
+  fact=1; //since factorial of 0 is 1.
+}
+else
+{
+for
+(i=1;i<=no;i++)
 {
 fact=fact*i;
+}  
 }
 cout<<"Factorial: "<<fact;
 getch();
