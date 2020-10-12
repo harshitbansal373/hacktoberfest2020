8aec7c8c6411687236ba93bc44e5bcb45452e10b
Muhammad Rizaldi <mrizaldi1998@gmail.com>
2020-10-12 09:59:06 +0800

    add simple function

---
+++ simple_function.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+void myFunction(string fname) {
+  cout << fname << " william\n";
+}
+
+int main() {
+  myFunction("aldo");
+  myFunction("budi");
+  myFunction("siti");
+  return 0;
+}
