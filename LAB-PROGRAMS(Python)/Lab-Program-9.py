979916dc9408abf787457ac9c2ced1a1ab7fc722
Harshit Bansal <harshitbansal373@gmail.com>
2020-10-13 10:55:41 +0530

    Delete Lab-Program-9.py

--- LAB-PROGRAMS(Python)/Lab-Program-9.py
+++
@@ -1,8 +0,0 @@
-class Print:
-     def __init__(self,name):
-          self.name=name
-    def in_class(self):
-        print("Function Inside Class")
-        print("I am writing in "+self.name)        
-Object=Print("Python")
-Object.in_class()
