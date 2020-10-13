e6c79b7bcabdd92a3ede8ce7c5cf942d95254bbd
Harshit Bansal <harshitbansal373@gmail.com>
2020-10-13 10:59:06 +0530

    Delete Lab-Program-2.py

--- LAB-PROGRAMS(Python)/Lab-Program-2.py
+++
@@ -1,8 +0,0 @@
-n=int(input("Enter the value of n:"))
-for i in range(2,n):
-    count=0
-    for j in range(2,i+1,1):
-        if(i%j==0):
-            count+=1
-    if(count<=1):
-        print(i)
