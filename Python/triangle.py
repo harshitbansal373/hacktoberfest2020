e540f5621a556171a94b87a706fca1ede1fa183f
Chibi Shem <carl.ipanag@gmail.com>
2020-10-18 16:11:37 +0800

    added triangle.py python script

---
+++ Python/triangle.py
@@ -0,0 +1,13 @@
+"""
+This script creates a triangle based on the input height.
+"""
+
+print('Enter triangle height:')
+height = input()
+for level in xrange(height):
+    output = ''
+    for space in xrange((height-1)-level):
+        output += ' '
+    for star in xrange((level*1)+(level+1)):
+        output += '*'
+    print(output)
