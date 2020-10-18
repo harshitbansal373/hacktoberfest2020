6930cd083d0388fa473f47c7376e3b33bc34f833
MeghanaPalkar <45203340+MeghanaPalkar@users.noreply.github.com>
2020-10-18 17:06:11 +0530

    Create Palindrome.c in C

---
+++ C/Palindrome.c
@@ -0,0 +1,35 @@
+/*
+Following is the code to Find if entered string is Palindrome or not
+
+*/
+
+#include <stdio.h> 
+#include <string.h> 
+  
+// A function to check if a string str is palindrome 
+void isPalindrome(char str[]) 
+{ 
+    // Start from leftmost and rightmost corners of str 
+    int l = 0; 
+    int h = strlen(str) - 1; 
+  
+    // Keep comparing characters while they are same 
+    while (h > l) 
+    { 
+        if (str[l++] != str[h--]) 
+        { 
+            printf("%s is Not Palindrome", str); 
+            return; 
+        } 
+    } 
+    printf("%s is palindrome", str); 
+} 
+  
+// Driver program to test above function 
+int main() 
+{ 
+    isPalindrome("abba"); 
+    isPalindrome("abbccbba"); 
+    isPalindrome("geeks"); 
+    return 0; 
+}
