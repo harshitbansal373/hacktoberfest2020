c51dc6ecc5791408b61f667bc42f729f86e5abc3
AyushTomar1 <73213195+AyushTomar1@users.noreply.github.com>
2020-10-21 11:28:25 +0530

    Update Count_Digits.c

--- C/Count_Digits.c
+++ C/Count_Digits.c
@@ -1,16 +1,16 @@
 #include<stdio.h>
-
+int countDigit(int n) 
+{ 
+    if (n == 0) 
+        return 0; 
+    return 1 + countDigit(n / 10); 
+} 
 int main()
 {
-    int number,count=0;
+    int number,count;
     printf("Enter a number:");
     scanf("%d",&number);
-
-    while(number!=0)
-    {
-        number/=10;
-        ++count;
-    }
+    count=countDigit(number);
     printf("%d",count);
     return 0;
 }
