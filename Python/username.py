c023da2a9f4876ed46944dd4e239e1b819ff8a3b
Dijo Daiju <60286610+dijodaiju7@users.noreply.github.com>
2020-10-22 11:49:02 +0530

    Added username.py

---
+++ Python/username.py
@@ -0,0 +1,11 @@
+import sys
+b=input("Enter your name")
+c=input("Enter your favourite number")
+d=input("Enter your star")
+sys.stdout.write(b)
+sys.stdout.write("@")
+sys.stdout.write(c)
+sys.stdout.write("_")
+sys.stdout.write(d)
+
+
