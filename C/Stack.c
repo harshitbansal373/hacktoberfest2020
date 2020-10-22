1486e30f309e00884e6b061c0e8fb88ecab0fb71
Amaan <69464744+AmaanSayyad@users.noreply.github.com>
2020-10-22 12:09:55 +0530

    Update Stack.c
    //Hacktoberfest-PR8//
    
    Updated Stack using switch case and added additional features to stack like peek(),display(),exit(), push() and pop()

--- C/Stack.c
+++ C/Stack.c
@@ -1,64 +1,81 @@
 // Implementation of Stack 
-// Operation : Push() and Pop()
+// Operation : Push(), Pop(), Peek(), Display(), Exit()
 
-#include <stdio.h>
-#define MAX 10
-int A[10];
-void push(int [],int,int*);
-int pop(int[],int*);
-void main()
-{
-	int top = 0,ch,num;
-	do
-	{
-	
-		printf("\n enter your choices \n1.Push operation \n2.Pop operration \n3.Exit\n");
-		scanf("%d",&ch);
-		switch(ch)
-		{
-			case 1:
-				printf("\n Pls enter  a num to be pushed : ");
-				scanf("%d",&num);
-				push(A,num,&top);
-				break;
-			case 2:
-				printf("\n Pls enter a number to be pop:");
-				scanf("%d",num);
-				break;
-			case 3:
-				break;
-			default:
-				printf("\n Wrong choise !");
-		}
-	}while(ch != 3);
+#include <iostream>
+using namespace std;
+int stack[100], n=100, top=-1;
+
+void push(int val) {
+   if(top>=n-1)
+   cout<<"Stack Overflow"<<endl;
+   else {
+      top++;
+      stack[top]=val;
+   }
 }
-void push(int A[],int num, int* top)
-{
-	if(*top == MAX)
-	{
-		printf("\n The stack is FULL !");
-		return;
-	}
-	else
-	{
-		*top = *top + 1;
-		A[*top] = num;
-		printf("\n The pushed element is %d and position is %d",num,*top);
-	}
+
+void pop() {
+   if(top<=-1)
+   cout<<"Stack Underflow"<<endl;
+   else {
+      cout<<"The popped element is "<< stack[top] <<endl;
+      top--;
+   }
 }
-int pop(int A[], int* top)
-{
-	int num;
-	if(*top == 0)
-	{
-		printf("\n The stack is empty !");
-		return;
-	}
-	else
-	{
-		A[*top] = num;
-		printf("\n The poped element is %d and position is %d",num,*top);
-		*top = *top - 1;
-		return(num);
-	}
+
+void peek() {
+   cout<< stack[top] << " is the peek element"<<endl;
+}
+
+void display() {
+   if(top>=0) {
+      cout<<"Stack elements are: ";
+      for(int i=top; i>=0; i--)
+      cout<<stack[i]<<" ";
+      cout<<endl;
+   } else
+   cout<<"Stack is empty";
+}
+
+
+int main() {
+   int ch, val;
+   cout<<"1) Push in stack"<<endl;
+   cout<<"2) Pop from stack"<<endl;
+   cout<<"3) Display stack"<<endl;
+   cout<<"4) Peek in stack"<<endl;
+   cout<<"5) Exit"<<endl;
+   do {
+      cout<<"Enter choice: "<<endl;
+      cin>>ch;
+      switch(ch) {
+         case 1: {
+            cout<<"Enter value to be pushed:"<<endl;
+            cin>>val;
+            push(val);
+            break;
+         }
+         case 2: {
+            pop();
+            break;
+         }
+         case 3: {
+            display();
+            break;
+         }
+         case 4: {
+            peek();
+            break;
+         }
+         case 5: {
+            cout<<"Exit"<<endl;
+            break;
+         }
+         default: {
+            cout<<"Invalid Choice"<<endl;
+         }
+      }
+   }while(ch!=5);
+   return 0;
+}
 }
