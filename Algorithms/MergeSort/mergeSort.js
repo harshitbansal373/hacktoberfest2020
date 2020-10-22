cd273c661f12d2dd6af0dcfa2ef6c7bdb0c8a55c
csteacher1 <knichols@rcs.rome.ga.us>
2020-10-22 17:39:13 -0400

    Create mergeSort.js

---
+++ Algorithms/MergeSort/mergeSort.js
@@ -0,0 +1,22 @@
+
+// Merge the two arrays: left and right
+function merge (left, right) {
+  let resultArray = [], leftIndex = 0, rightIndex = 0;
+
+  // We will concatenate values into the resultArray in order
+  while (leftIndex < left.length && rightIndex < right.length) {
+    if (left[leftIndex] < right[rightIndex]) {
+      resultArray.push(left[leftIndex]);
+      leftIndex++; // move left array cursor
+    } else {
+      resultArray.push(right[rightIndex]);
+      rightIndex++; // move right array cursor
+    }
+  }
+
+  // We need to concat here because there will be one element remaining
+  // from either left OR the right
+  return resultArray
+          .concat(left.slice(leftIndex))
+          .concat(right.slice(rightIndex));
+}
