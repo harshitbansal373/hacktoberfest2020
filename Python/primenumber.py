1405594637da066dd0ca6d6666c3286fc62fd23e
Bünyamin Mete <metebunyaminn@gmail.com>
2020-10-23 13:10:25 +0300

    Update primenumber.py

--- Python/primenumber.py
+++ Python/primenumber.py
@@ -1,13 +1,11 @@
 def isprime(x):
     prime = True
-    
     if x ==2:
         prime = True
         return prime
     if x%2 == 0 :
         prime= False
         return prime
-           
     for y in range(3,x,2):
         if x%y ==0:
             prime = False
