2eba97a79a1c1bb44e6de92a199c9fcc662b95ef
Harshita-kishore17 <48876769+Harshita-kishore17@users.noreply.github.com>
2020-10-24 20:35:16 +0530

    Update binarySearch.cpp

--- binarySearch.cpp
+++ binarySearch.cpp
@@ -4,14 +4,14 @@
 using namespace std;
 int binarySearch(int arr[], int l, int r, int x)
 {
-	if (r >= l)
+	if (r >= l)   // r is max range and l is min range
 	{
 		int mid = l + (r - l) / 2;
 		if (arr[mid] == x)
 			return mid;
 		if (arr[mid] > x)
-			return binarySearch(arr, l, mid - 1, x);
-		return binarySearch(arr, mid + 1, r, x);
+			return binarySearch(arr, l, mid - 1, x); // goes to search in left side of array.
+		return binarySearch(arr, mid + 1, r, x);	// goes to search in right side of array.
 	}
 	return -1;
 }
