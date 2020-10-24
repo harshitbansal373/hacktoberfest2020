958e7768f47985dcb300013e2b9c8f3cd0f4d8ee
Prithu-Nagar <56398437+Prithu-Nagar@users.noreply.github.com>
2020-10-24 19:18:46 +0530

    Update bubbleSort.py

--- Python/bubbleSort.py
+++ Python/bubbleSort.py
@@ -1,22 +1,19 @@
-# Python program for implementation of Bubble Sort 
+def bubble(L):
+    for i in range(len(L)-1):
+        print('\nthis is the ',i,'pass')
+        time.sleep(1)
+        for j in range(len(L)-1-i):
+            time.sleep(1)
+            print('j is',L[j],'j+1 is',L[j+1])
+            if L[j]>L[j+1]:
+                print('Swap hoga')
+                L[j],L[j+1]=L[j+1],L[j]
+                print('After swap',L)
+        print('After the pass',L)
+    print(L)
 
-def bubbleSort(arr): 
-	n = len(arr) 
-
-	# Traverse through all array elements 
-	for i in range(n): 
-
-		# Last i elements are already in place 
-		for j in range(0, n-i-1): 
- 
-			if arr[j] > arr[j+1] : 
-				arr[j], arr[j+1] = arr[j+1], arr[j] 
-
-# Driver code to test above 
-arr = [64, 34, 25, 12, 22, 11, 90] 
-
-bubbleSort(arr) 
-
-print ("Sorted array is:") 
-for i in range(len(arr)): 
-	print ("%d" %arr[i]), 
+l=[]
+n=int(input("Enter the size of array"))
+for i in range(n):
+    l.append(int(input()))
+bubble(l)
