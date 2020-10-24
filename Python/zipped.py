9458479337bab6bda1f729991ac42cf35e1934d8
Yash Sanghvi <56947924+yashsanghvi001@users.noreply.github.com>
2020-10-24 12:45:20 +0530

    Create zipped.py

---
+++ Python/zipped.py
@@ -0,0 +1,11 @@
+
+if __name__ == "__main__":
+    st_num, sb_num = map(int, input().strip().split())
+    scores = []
+    
+    for _ in range(sb_num):
+        scores.append(map(float, input().strip().split()))
+        
+    for el in zip(*scores):
+        print(sum(el)/sb_num)
+        
