dac9a54332db846ece7b88840d33c578e9e3cbf0
Prithu-Nagar <56398437+Prithu-Nagar@users.noreply.github.com>
2020-10-24 19:11:20 +0530

    Update Binary-search.py

--- Python/Binary-search.py
+++ Python/Binary-search.py
@@ -1,24 +1,22 @@
-"""
-@author Jeppe Kristensen
-jepify
-"""
+def b(a,x,l,h):
+    mid=(l+h)//2
+    if(l>h):
+        return 0
+    elif(x==a[mid]):
+        return mid
+    elif x>a[mid]:
+        return b(a,x,mid+1,h) 
+    else:
+        return b(a,x,l,mid-1)
+    
 
-def binarySearch(l, n, start, end):
-    if start == end:
-        return l[start] == n
-
-    test_index = (end - start) // 2 + start
-    test_number = l[test_index]
-    if test_number == n:
-        return True
-    elif n < test_number:
-        return binarySearch(l, n, start, test_index - 1)
-    elif n > test_number:
-        return binarySearch(l, n, test_index + 1, end)
-
-
-
-l = [1,2,3,4,5,7,8,9,10]
-
-print(f"{binarySearch(l,5, 0, len(l) - 1)=}")
-print(f"{binarySearch(l,6, 0, len(l) - 1)=}")
+n=int(input("Enter the size of array:"))
+a=input("Enter the elements in space separated form:").split()
+a=[int(x) for x in a]
+x=int(input("Enter the element to be searched:"))
+low=0
+high=len(a)-1
+if(b(a,x,low,high)):
+    print("\nThe element is found at index:",b(a,x,low,high))
+else:
+    print("\nElement not found")
