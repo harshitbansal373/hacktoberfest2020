6e9baf262cb7b2e73676a4d536e036de26968b64
RathodBhakti <55892408+RathodBhakti@users.noreply.github.com>
2020-10-26 18:15:58 +0530

    Added 2 files

---
+++ C/bufferoverflow.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int i,a[]={4,9,3,6,1};
+    int b[10];
+    printf("Enter 2nd array elemenets:\n");
+    for(i=0;i<10;i++)
+    {
+    scanf("%d",&b[i]);
+    }
+    return 0;
+}
