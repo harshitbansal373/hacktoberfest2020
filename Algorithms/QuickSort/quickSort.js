2c38d09b48ae5771a782aff92dbaef7964cd626c
Cristian408 <72926176+Cristian408@users.noreply.github.com>
2020-10-26 08:55:28 -0400

    Create quickSort.js

---
+++ Algorithms/QuickSort/quickSort.js
@@ -0,0 +1,19 @@
+function quickSortBasic(array) {
+  if(array.length < 2) {
+    return array;
+  }
+
+  var pivot = array[0];
+  var lesserArray = [];
+  var greaterArray = [];
+
+  for (var i = 1; i < array.length; i++) {
+    if ( array[i] > pivot ) {
+      greaterArray.push(array[i]);
+    } else {
+      lesserArray.push(array[i]);
+    }
+  }
+
+  return quickSortBasic(lesserArray).concat(pivot, quickSortBasic(greaterArray));
+}
