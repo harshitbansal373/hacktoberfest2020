876dcb106a3819b7a446823cf907b5ec06827dd1
BaguetteDev <72872790+BaguetteDev@users.noreply.github.com>
2020-10-26 10:01:06 -0400

    Create mergeSort2.js

---
+++ Algorithms/MergeSort/mergeSort2.js
@@ -0,0 +1,21 @@
+
+function merge (left, right) {
+  let resultArray = [], leftIndex = 0, rightIndex = 0;
+
+  while (leftIndex < left.length && rightIndex < right.length) {
+    if (left[leftIndex] < right[rightIndex]) {
+      resultArray.push(left[leftIndex]);
+      leftIndex++; // move left array cursor
+    } else {
+      resultArray.push(right[rightIndex]);
+      rightIndex++; // move right array cursor
+    }
+  }
+
+
+  return resultArray
+          .concat(left.slice(leftIndex))
+          .concat(right.slice(rightIndex));
+}
+
+//credit to Tim Han
