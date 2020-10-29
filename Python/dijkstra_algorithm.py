b1072a13b447f41162f1d7648e4712b2edd1275d
Paulo Octávio Araújo de Paula <paulooctavio@usp.br>
2020-10-28 23:53:36 -0300

    Create dijkstra_algorithm.py

---
+++ Python/dijkstra_algorithm.py
@@ -0,0 +1,16 @@
+from pythonds.graphs import PriorityQueue, Graph, Vertex
+def dijkstra(aGraph,start):
+    pq = PriorityQueue()
+    start.setDistance(0)
+    pq.buildHeap([(v.getDistance(),v) for v in aGraph])
+    while not pq.isEmpty():
+        currentVert = pq.delMin()
+        for nextVert in currentVert.getConnections():
+            newDist = currentVert.getDistance() \
+                    + currentVert.getWeight(nextVert)
+            if newDist < nextVert.getDistance():
+                nextVert.setDistance( newDist )
+                nextVert.setPred(currentVert)
+                pq.decreaseKey(nextVert,newDist)
+                
+#Source: https://panda.ime.usp.br/pythonds/static/pythonds_pt/07-Grafos/DijkstrasAlgorithm.html
