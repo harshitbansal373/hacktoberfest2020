3eb0561e0ada6ac826c2b3510aff0d878cb4f7e7
Andrew Chudinovskyh <andron989@gmail.com>
2020-10-29 21:45:33 +0200

    Added isNumber isOdd isEven functions

---
+++ JavaScript/IsOddIsEven.js
@@ -0,0 +1,33 @@
+function isNumber(num) {
+  if (typeof num === 'number') {
+    return num - num === 0;
+  }
+  if (typeof num === 'string' && num.trim() !== '') {
+    return Number.isFinite ? Number.isFinite(+num) : isFinite(+num);
+  }
+  return false;
+}
+
+function isOdd(value) {
+  const n = Math.abs(value);
+  if (!isNumber(n)) {
+    throw new TypeError('expected a number');
+  }
+  if (!Number.isInteger(n)) {
+    throw new Error('expected an integer');
+  }
+  if (!Number.isSafeInteger(n)) {
+    throw new Error('value exceeds maximum safe integer');
+  }
+  return (n % 2) === 1;
+}
+
+function isEven(i) {
+  return !isOdd(i);
+}
+
+module.exports = {
+  isOdd,
+  isEven,
+  isNumber
+}
