284dd6610a2fcb1daa227109b5b599a0780881ba
harmeetsinghjsr <73695667+harmeetsinghjsr@users.noreply.github.com>
2020-10-30 19:43:04 +0530

    Create glitchcode.java
    It's working code

---
+++ Java/glitchcode.java
@@ -0,0 +1,17 @@
+private static void printFileJava7() throws IOException {
+
+    try(FileInputStream input = new FileInputStream("file.txt")) {
+
+        int data = input.read();
+
+        while(data != -1){
+
+            System.out.print((char) data);
+
+            data = input.read();
+
+        }
+
+    }
+
+}
