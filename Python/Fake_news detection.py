f53546efa5e04c2c7e539bdfc27e922c52583eac
vishwajeet009 <56509693+vishwajeet009@users.noreply.github.com>
2020-10-31 01:47:26 -0700

    Create Fake_news detection.py

---
+++ Python/Fake_news detection.py
@@ -0,0 +1,40 @@
+import numpy as np
+import pandas as pd
+import itertools
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.linear_model import PassiveAggressiveClassifier
+from sklearn.metrics import accuracy_score, confusion_matrix
+
+#Read the data
+df=pd.read_csv('D:\\DataFlair\\news.csv')
+
+#Get shape and head
+df.shape
+df.head()
+
+#DataFlair - Get the labels
+labels=df.label
+labels.head()
+
+#DataFlair - Split the dataset
+x_train,x_test,y_train,y_test=train_test_split(df['text'], labels, test_size=0.2, random_state=7)
+
+#DataFlair - Initialize a TfidfVectorizer
+tfidf_vectorizer=TfidfVectorizer(stop_words='english', max_df=0.7)
+
+#DataFlair - Fit and transform train set, transform test set
+tfidf_train=tfidf_vectorizer.fit_transform(x_train) 
+tfidf_test=tfidf_vectorizer.transform(x_test)
+
+#DataFlair - Initialize a PassiveAggressiveClassifier
+pac=PassiveAggressiveClassifier(max_iter=50)
+pac.fit(tfidf_train,y_train)
+
+#DataFlair - Predict on the test set and calculate accuracy
+y_pred=pac.predict(tfidf_test)
+score=accuracy_score(y_test,y_pred)
+print(f'Accuracy: {round(score*100,2)}%')
+
+#DataFlair - Build confusion matrix
+confusion_matrix(y_test,y_pred, labels=['FAKE','REAL'])
