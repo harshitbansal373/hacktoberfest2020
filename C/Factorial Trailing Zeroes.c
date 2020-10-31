e734078a63bd81c95c9370687c3fb97275442e17
yanand <yanand@adobe.com>
2020-10-31 23:31:57 +0530

    Factorial Trailing Zeroes

---
+++ C/Factorial Trailing Zeroes.c
@@ -0,0 +1,13 @@
+// Given an integer n, return the number of trailing zeroes in n!.
+
+int solution(){
+    int trailingZeroes(int n) {
+	long long i=5;
+    int count=0;
+	while(n/i>=1){
+	    count+=n/i;
+	    i=i*5;
+	}
+	return count;
+    }
+};
